cc743be911bed52da0c31ff87ea2c7359cd87253
Nick Kopilovskii <nkopilovskii@gmail.com>
2019-07-11 14:20:37 +0300

    created example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nick Kopilovskii on 7/11/19.
+//  Copyright © 2019  Nick Kopilovskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
